26d21b1a176316b8b76d594c57ef6b89899369ee
clickpen <37470327+clickpen@users.noreply.github.com>
2018-03-27 13:19:53 +0800

    Add files via upload

---
+++ css+jquery实现图片展示/js/click.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+	$('.stacks-cards').on('click',function() {
+		$('.stacks-cards').toggleClass('transition')
+	})
+
+
+	// var stacks = document.getElementById('stacks-cards');
+	// stacks.addEventListener('click',change);
+	// function change() {
+	// 	stacks.classList.add('transition');
+	// }
